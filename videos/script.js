f9ba2da8eb3caffff8b5c47f7ae80153d8ce7c10
Dujexs <145869099+DujexsYT@users.noreply.github.com>
2023-10-26 22:00:06 -0300

    Update script.js

--- videos/script.js
+++ videos/script.js
@@ -1,11 +1,3 @@
-/*
-🎬 Video playlist UI Design like Skillshare With Vanilla JavaScript
-👨🏻‍⚕️ By: Coding Design
-
-You can do whatever you want with the code. However if you love my content, you can subscribed my YouTube Channel
-🌎link: www.youtube.com/codingdesign
-*/
-
 const main_video = document.querySelector('.main-video video');
 const main_video_title = document.querySelector('.main-video .title');
 const video_playlist = document.querySelector('.video-playlist .videos');
